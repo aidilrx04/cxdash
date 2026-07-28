564ce1532d5b79c3206dcf0bbd6415e28a301885
aidilrx04 <mrcrayv14@gmail.com>
2026-07-29 01:58:34 +0800

    add ai skills

---
+++ app/Filament/Resources/Trainers/RelationManagers/AiSkillsRelationManager.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Filament\Resources\Trainers\RelationManagers;
+
+use Filament\Actions\AssociateAction;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\CreateAction;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\DissociateAction;
+use Filament\Actions\DissociateBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Forms\Components\Textarea;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Schemas\Schema;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class AiSkillsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'aiSkills';
+
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Textarea::make('ai_usage')
+                    ->required()
+                    ->columnSpanFull(),
+                Textarea::make('ai_tools')
+                    ->required()
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public function infolist(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextEntry::make('ai_usage')
+                    ->columnSpanFull(),
+                TextEntry::make('ai_tools')
+                    ->columnSpanFull(),
+                TextEntry::make('created_at')
+                    ->dateTime()
+                    ->placeholder('-'),
+                TextEntry::make('updated_at')
+                    ->dateTime()
+                    ->placeholder('-'),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('ai_usage')
+            ->columns([
+                TextColumn::make('ai_usage')
+                    ->wrap(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                CreateAction::make(),
+                AssociateAction::make(),
+            ])
+            ->recordActions([
+                ViewAction::make(),
+                EditAction::make(),
+                DissociateAction::make(),
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DissociateBulkAction::make(),
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
--- app/Filament/Resources/Trainers/TrainerResource.php
+++ app/Filament/Resources/Trainers/TrainerResource.php
@@ -6,6 +6,7 @@ use App\Filament\Resources\Trainers\Pages\CreateTrainer;
 use App\Filament\Resources\Trainers\Pages\EditTrainer;
 use App\Filament\Resources\Trainers\Pages\ListTrainers;
 use App\Filament\Resources\Trainers\Pages\ViewTrainer;
+use App\Filament\Resources\Trainers\RelationManagers\AiSkillsRelationManager;
 use App\Filament\Resources\Trainers\RelationManagers\CertificatesRelationManager;
 use App\Filament\Resources\Trainers\RelationManagers\EducationRelationManager;
 use App\Filament\Resources\Trainers\RelationManagers\IndustriesRelationManager;
@@ -59,6 +60,7 @@ class TrainerResource extends Resource
             ToolsRelationManager::class,
             TrainingMethodsRelationManager::class,
             LanguagesRelationManager::class,
+            AiSkillsRelationManager::class,
         ];
     }
 
