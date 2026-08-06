9136769297f2fe3a8ca627711018c25629f46064
aidilrx04 <mrcrayv14@gmail.com>
2026-08-06 11:50:29 +0800

    refactor sentiment response

--- app/Filament/Resources/TrainingReports/RelationManagers/FeedbackGeneralsRelationManager.php
+++ app/Filament/Resources/TrainingReports/RelationManagers/FeedbackGeneralsRelationManager.php
@@ -16,6 +16,7 @@ use Filament\Forms\Components\TextInput;
 use Filament\Notifications\Notification;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Schemas\Schema;
+use Filament\Support\Colors\Color;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
@@ -45,25 +46,29 @@ class FeedbackGeneralsRelationManager extends RelationManager
             TextColumn::make('name')
         ];
 
+
         $feedbackGenerals = $this->getOwnerRecord()->feedbackGenerals->first()->feedbackGenerals;
         foreach ($questions as $question) {
             $schema[] = TextColumn::make('feedback_question' . Str::uuid())
                 ->label($question->question)
                 ->getStateUsing(function ($record) use ($question) {
                     // dd($record->feedbackGenerals()->where('feedback_question_id', $question->id)->first());
-                    return $record->feedbackGenerals()->where('feedback_question_id', $question->id)->first()->response;
+                    $responseCount = $record->feedbackGenerals()->count() > 0;
+                    return $responseCount ? ($record->feedbackGenerals()->where('feedback_question_id', $question->id)->first()->response ?? '') : 'No Response';
                 });
             $schema[] = TextColumn::make('feedback_sentiment' . Str::uuid())
                 ->label('Sentiment')
                 ->getStateUsing(function ($record) use ($question) {
                     // dd($record->feedbackGenerals()->where('feedback_question_id', $question->id)->first());
-                    return $record->feedbackGenerals()->where('feedback_question_id', $question->id)->first()->sentiment;
+                    $responseCount = $record->feedbackGenerals()->count() > 0;
+                    return $responseCount ? ($record->feedbackGenerals()->where('feedback_question_id', $question->id)->first()->sentiment ?? '') : 'No Response';
                 });
             $schema[] = TextColumn::make('feedback_theme' . Str::uuid())
                 ->label('Theme')
                 ->getStateUsing(function ($record) use ($question) {
                     // dd($record->feedbackGenerals()->where('feedback_question_id', $question->id)->first());
-                    return $record->feedbackGenerals()->where('feedback_question_id', $question->id)->first()->theme;
+                    $responseCount = $record->feedbackGenerals()->count() > 0;
+                    return $responseCount ? ($record->feedbackGenerals()->where('feedback_question_id', $question->id)->first()->theme ?? '') : 'No Response';
                 });
         }
         // foreach()
