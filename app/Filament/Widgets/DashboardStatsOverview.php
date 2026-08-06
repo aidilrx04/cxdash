600ebb4c161e7f2da4ce986753b6e4e02029a315
aidilrx04 <mrcrayv14@gmail.com>
2026-08-06 18:55:19 +0800

    more widgets

--- app/Filament/Widgets/DashboardStatsOverview.php
+++ app/Filament/Widgets/DashboardStatsOverview.php
@@ -9,10 +9,21 @@ use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class DashboardStatsOverview extends StatsOverviewWidget
 {
+
+    protected function getHeading(): ?string
+    {
+        return 'Analytics';
+    }
+
+    protected function getDescription(): ?string
+    {
+        return 'An overview of customer experience analytics.';
+    }
     protected function getStats(): array
     {
         return [
-            Stat::make('Training', TrainingReport::count()),
+            Stat::make('Training', TrainingReport::count())
+                ->color('success'),
             Stat::make('Active Trainers', Trainer::count()),
             Stat::make('Active Trainers', TrainingReport::sum('total_participants')),
             Stat::make('Avg PSS', TrainingReport::avg('pss_score')),
