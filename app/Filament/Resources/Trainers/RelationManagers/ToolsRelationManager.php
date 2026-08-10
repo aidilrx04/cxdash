81d93adb0b1f1ce088c326f049ed38b642963775
aidilrx04 <mrcrayv14@gmail.com>
2026-08-11 01:44:00 +0800

    tools relation

--- app/Filament/Resources/Trainers/RelationManagers/ToolsRelationManager.php
+++ app/Filament/Resources/Trainers/RelationManagers/ToolsRelationManager.php
@@ -2,6 +2,8 @@
 
 namespace App\Filament\Resources\Trainers\RelationManagers;
 
+use App\Models\Tool;
+use BackedEnum;
 use Filament\Actions\AttachAction;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\CreateAction;
@@ -14,6 +16,7 @@ use Filament\Actions\ViewAction;
 use Filament\Forms\Components\TextInput;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
@@ -22,12 +25,34 @@ class ToolsRelationManager extends RelationManager
 {
     protected static string $relationship = 'tools';
 
+    protected static ?string $title = 'Software, Hardware & Tech Tools';
+
+    protected static string|BackedEnum|null $icon = 'heroicon-m-wrench-screwdriver';
+
     public function form(Schema $schema): Schema
     {
         return $schema
             ->components([
                 TextInput::make('name')
-                    ->required(),
+                    ->label('Tool / Software / Platform Name')
+                    ->placeholder('e.g., Cisco Packet Tracer, Wireshark, Laravel, Figma, Docker')
+                    ->prefixIcon('heroicon-m-wrench')
+                    ->datalist([
+                        'Cisco Packet Tracer / GNS3',
+                        'Wireshark / Network Analyzers',
+                        'Laravel / Filament PHP',
+                        'Svelte / SvelteKit',
+                        'Docker / Kubernetes',
+                        'Git / GitHub / GitLab',
+                        'Postman / Insomnia API Client',
+                        'Figma / Penpot / UI Design Tools',
+                        'VS Code / JetBrains IDEs',
+                        'Linux / Bash / Shell Scripting',
+                        'Jira / Confluence / Trello',
+                        'AWS / Google Cloud Platform / Azure',
+                    ])
+                    ->required()
+                    ->columnSpanFull(),
             ]);
     }
 
@@ -35,13 +60,28 @@ class ToolsRelationManager extends RelationManager
     {
         return $schema
             ->components([
-                TextEntry::make('name'),
-                TextEntry::make('created_at')
-                    ->dateTime()
-                    ->placeholder('-'),
-                TextEntry::make('updated_at')
-                    ->dateTime()
-                    ->placeholder('-'),
+                Section::make('Tool Details')
+                    ->icon('heroicon-m-wrench-screwdriver')
+                    ->columns(2)
+                    ->columnSpanFull()
+                    ->schema([
+                        TextEntry::make('name')
+                            ->label('Tool / Technology')
+                            ->badge()
+                            ->color('info')
+                            ->icon('heroicon-m-wrench')
+                            ->columnSpanFull(),
+
+                        TextEntry::make('created_at')
+                            ->label('Added On')
+                            ->dateTime('d M Y, h:i A')
+                            ->placeholder('-'),
+
+                        TextEntry::make('updated_at')
+                            ->label('Last Updated')
+                            ->dateTime('d M Y, h:i A')
+                            ->placeholder('-'),
+                    ]),
             ]);
     }
 
@@ -49,15 +89,27 @@ class ToolsRelationManager extends RelationManager
     {
         return $table
             ->recordTitleAttribute('name')
+            ->defaultSort('name', 'asc')
+            ->striped()
             ->columns([
                 TextColumn::make('name')
-                    ->searchable(),
+                    ->label('Tool / Technology')
+                    ->searchable()
+                    ->sortable()
+                    ->badge()
+                    ->color('info')
+                    ->icon('heroicon-m-wrench')
+                    ->weight('bold'),
+
                 TextColumn::make('created_at')
-                    ->dateTime()
+                    ->label('Date Added')
+                    ->dateTime('d M Y')
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
+
                 TextColumn::make('updated_at')
-                    ->dateTime()
+                    ->label('Last Modified')
+                    ->dateTime('d M Y')
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
             ])
@@ -65,20 +117,33 @@ class ToolsRelationManager extends RelationManager
                 //
             ])
             ->headerActions([
-                CreateAction::make(),
-                AttachAction::make(),
+                AttachAction::make()
+                    ->label('Attach Existing')
+                    ->icon('heroicon-m-link')
+                    ->preloadRecordSelect()
+                    ->modalHeading('Attach Tool / Technology to Trainer'),
+
+                CreateAction::make()
+                    ->label('Create New')
+                    ->icon('heroicon-m-plus')
+                    ->modalHeading('Create & Attach New Tool'),
             ])
             ->recordActions([
-                ViewAction::make(),
-                EditAction::make(),
-                DetachAction::make(),
-                DeleteAction::make(),
+                ViewAction::make()->iconButton(),
+                EditAction::make()->iconButton(),
+                DetachAction::make()
+                    ->iconButton()
+                    ->tooltip('Detach from trainer'),
+                DeleteAction::make()->iconButton(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
                     DetachBulkAction::make(),
                     DeleteBulkAction::make(),
                 ]),
-            ]);
+            ])
+            ->emptyStateIcon('heroicon-o-wrench-screwdriver')
+            ->emptyStateHeading('No Tools Linked')
+            ->emptyStateDescription('Attach or create software, hardware, or framework tools (e.g., Cisco Packet Tracer, Laravel, Figma) that this trainer uses.');
     }
 }
